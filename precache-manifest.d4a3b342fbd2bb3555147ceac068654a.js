23da6eb23b6c4dec80527baddfa3a2a564f4146b
Ankush Kumar <j268sing@edu.uwaterloo.ca>
2021-07-14 21:03:19 -0400

    Updates

---
+++ precache-manifest.d4a3b342fbd2bb3555147ceac068654a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc44186212b7476486139fd30cd4b9b5",
+    "url": "/soca-host/index.html"
+  },
+  {
+    "revision": "2c3500fe3b05b949a383",
+    "url": "/soca-host/static/css/main.86da89df.chunk.css"
+  },
+  {
+    "revision": "1459b6038a6088045565",
+    "url": "/soca-host/static/js/2.2b6cd696.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/soca-host/static/js/2.2b6cd696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3500fe3b05b949a383",
+    "url": "/soca-host/static/js/main.40f73bbb.chunk.js"
+  },
+  {
+    "revision": "4c4569c2088a5ba2f75d",
+    "url": "/soca-host/static/js/runtime-main.7093ac20.js"
+  },
+  {
+    "revision": "4e11072c0bbd8093bb5f9339ea645da2",
+    "url": "/soca-host/static/media/SOCAlogo.4e11072c.png"
+  },
+  {
+    "revision": "d2d7aabe1e036f9720fb03b4ef67696e",
+    "url": "/soca-host/static/media/batball.d2d7aabe.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/soca-host/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "a2fd6288e227c74a20beec18221a62f6",
+    "url": "/soca-host/static/media/dhruv.a2fd6288.jpg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/soca-host/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/soca-host/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/soca-host/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/soca-host/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/soca-host/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/soca-host/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/soca-host/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/soca-host/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/soca-host/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "f8da54e1aa9d00c63a226cf84f7c21da",
+    "url": "/soca-host/static/media/gagandeepsingh.f8da54e1.jpeg"
+  },
+  {
+    "revision": "62dce1413cffcefb1d686bb9d7d174ca",
+    "url": "/soca-host/static/media/ground1.62dce141.jpeg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/soca-host/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/soca-host/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/soca-host/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "f9233bbfb9cdf9efddd3c82c1fe5937f",
+    "url": "/soca-host/static/media/sharma.f9233bbf.jpg"
+  },
+  {
+    "revision": "e776a83448aa5d6ee2558009bb2aef71",
+    "url": "/soca-host/static/media/sunrisephoto.e776a834.jpg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/soca-host/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
